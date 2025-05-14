7fe4ff3610e02e264e834632b9f674c690646713
noirfate <test@example.com>
2025-05-14 15:07:43 +0800

    add huaweicloud chatbot agent

--- pyrit/prompt_target/__init__.py
+++ pyrit/prompt_target/__init__.py
@@ -26,6 +26,7 @@ from pyrit.prompt_target.openai.openai_tts_target import OpenAITTSTarget
 from pyrit.prompt_target.playwright_target import PlaywrightTarget
 from pyrit.prompt_target.prompt_shield_target import PromptShieldTarget
 from pyrit.prompt_target.text_target import TextTarget
+from pyrit.prompt_target.pangu_target import PanguAgentTarget
 
 __all__ = [
     "AzureBlobStorageTarget",
@@ -50,4 +51,5 @@ __all__ = [
     "PromptTarget",
     "RealtimeTarget",
     "TextTarget",
+    "PanguAgentTarget"
 ]
